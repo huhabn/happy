dd79479f227a057b9234a59b95a15e8e864f84eb
Transformer <hah561651@outlook.com>
2025-09-18 13:55:18 +0800

    Add files via upload

---
+++ upload_file_entity.py
@@ -0,0 +1,4 @@
+# 允许上传的文件类型
+ALLOWED_IMAGE_EXTENSION = ["jpg", "jepg", "png", "webp", "gif", "svg"]
+ALLOWED_DOCUMENT_EXTENSION = ["markdown", "md", "doc", "docx", "txt", "pdf", "csv", "xlsx", "xls", "html", "htm"]
+
